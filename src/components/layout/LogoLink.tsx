439f67df241bee13a45eea662c931c0dd5873272
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-01-20 19:13:03 +0100

    fix: added sitemap + fixed accessibility label for logolink

--- src/components/layout/LogoLink.tsx
+++ src/components/layout/LogoLink.tsx
@@ -21,7 +21,7 @@ function LogoLink({
       variant='none'
       size='none'
     >
-      <Link href='/' aria-label={t('home')} onClick={onClick}>
+      <Link href='/' aria-label={t('hackerspaceHome')} onClick={onClick}>
         <Logo className='h-6 w-6 md:h-8 md:w-8 xl:h-10 xl:w-10' />
         <span className='text-md font-montserrat font-bold md:text-lg xl:text-2xl'>
           HACKERSPACE
