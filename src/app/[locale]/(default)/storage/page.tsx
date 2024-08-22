06cf66d17af6b53a6c397ca45bef51c608fbbfc9
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2024-08-22 19:41:58 +0200

    feat: Add pagination to storage page

--- src/app/[locale]/(default)/storage/page.tsx
+++ src/app/[locale]/(default)/storage/page.tsx
@@ -2,7 +2,9 @@ import { items } from '@/mock-data/items';
 import { useTranslations } from 'next-intl';
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 import Image from 'next/image';
+import { createSearchParamsCache, parseAsInteger } from 'nuqs/parsers';
 
+import { PaginationCarousel } from '@/components/layout/PaginationCarousel';
 import {
   Card,
   CardContent,
@@ -28,11 +30,22 @@ export async function generateMetadata({
 
 export default function StoragePage({
   params: { locale },
+  searchParams,
 }: {
   params: { locale: string };
+  searchParams: Record<string, string | string[] | undefined>;
 }) {
   unstable_setRequestLocale(locale);
   const t = useTranslations('storage');
+  const t_ui = useTranslations('ui');
+
+  const itemsPerPage = 12;
+
+  const searchParamsCache = createSearchParamsCache({
+    [t_ui('page')]: parseAsInteger.withDefault(1),
+  });
+
+  const { [t_ui('page')]: page = 1 } = searchParamsCache.parse(searchParams);
 
   const categories = [
     {
@@ -64,34 +77,48 @@ export default function StoragePage({
         />
       </div>
       <div className='grid grid-cols-4 gap-3'>
-        {items.map((item) => (
-          <Card key={item.name} className='text-center'>
-            <CardHeader>
-              <CardTitle className='truncate'>{item.name}</CardTitle>
-              <CardDescription className='flex flex-col gap-1'>
-                <span>
-                  {t('card.quantityInfo', { quantity: item.quantity })}
-                </span>
-                <span>
-                  {t('card.locationInfo', { location: item.location })}
-                </span>
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Image
-                src='/unknown.png'
-                width={192}
-                height={192}
-                alt={`Photo of ${item.name}`}
-                className='mx-auto'
-              />
-            </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
-        ))}
+        {items
+          .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+          .map((item) => (
+            <Card key={item.name} className='text-center'>
+              <CardHeader>
+                <CardTitle className='truncate'>{item.name}</CardTitle>
+                <CardDescription className='flex flex-col gap-1'>
+                  <span>
+                    {t('card.quantityInfo', { quantity: item.quantity })}
+                  </span>
+                  <span>
+                    {t('card.locationInfo', { location: item.location })}
+                  </span>
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Image
+                  src='/unknown.png'
+                  width={192}
+                  height={192}
+                  alt={`Photo of ${item.name}`}
+                  className='mx-auto'
+                />
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          ))}
       </div>
+      <PaginationCarousel
+        className='my-6'
+        totalPages={Math.ceil(items.length / itemsPerPage)}
+        t={{
+          goToPreviousPage: t_ui('goToPreviousPage'),
+          previous: t_ui('previous'),
+          morePages: t_ui('morePages'),
+          goToNextPage: t_ui('goToNextPage'),
+          next: t_ui('next'),
+          page: t_ui('page'),
+        }}
+      />
     </>
   );
 }
