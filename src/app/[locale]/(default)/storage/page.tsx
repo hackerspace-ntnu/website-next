54fdd71b79496493c021822dc01b531c785a3efb
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2024-08-22 19:27:58 +0200

    feat: Add cards to storage page

--- src/app/[locale]/(default)/storage/page.tsx
+++ src/app/[locale]/(default)/storage/page.tsx
@@ -1,6 +1,16 @@
+import { items } from '@/mock-data/items';
 import { useTranslations } from 'next-intl';
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+import Image from 'next/image';
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/Card';
 import { Combobox } from '@/components/ui/Combobox';
 import { SearchBar } from '@/components/ui/SearchBar';
 
@@ -45,12 +55,43 @@ export default function StoragePage({
   return (
     <>
       <h1>{t('title')}</h1>
-      <SearchBar />
-      <Combobox
-        choices={categories}
-        defaultDescription={t('combobox.defaultDescription')}
-        defaultPlaceholder={t('combobox.defaultPlaceholder')}
-      />
+      <div className='my-4 flex justify-center gap-2'>
+        <SearchBar className='max-w-2xl' />
+        <Combobox
+          choices={categories}
+          defaultDescription={t('combobox.defaultDescription')}
+          defaultPlaceholder={t('combobox.defaultPlaceholder')}
+        />
+      </div>
+      <div className='grid grid-cols-4 gap-3'>
+        {items.map((item) => (
+          <Card key={item.name} className='text-center'>
+            <CardHeader>
+              <CardTitle className='truncate'>{item.name}</CardTitle>
+              <CardDescription className='flex flex-col gap-1'>
+                <span>
+                  {t('card.quantityInfo', { quantity: item.quantity })}
+                </span>
+                <span>
+                  {t('card.locationInfo', { location: item.location })}
+                </span>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Image
+                src='/unknown.png'
+                width={192}
+                height={192}
+                alt={`Photo of ${item.name}`}
+                className='mx-auto'
+              />
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </>
   );
 }
