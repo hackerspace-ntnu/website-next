bee1c233d0cbab597777f278de5526c0280b68eb
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2024-09-07 02:28:19 +0200

    feat: Add a back to storage button on the shopping cart page

--- src/app/[locale]/(default)/storage/shopping-cart/page.tsx
+++ src/app/[locale]/(default)/storage/shopping-cart/page.tsx
@@ -1,5 +1,8 @@
 import { ShoppingCartTable } from '@/components/storage/ShoppingCartTable';
 import ShoppingCartTableSkeleton from '@/components/storage/ShoppingCartTableSkeleton';
+import { Button } from '@/components/ui/Button';
+import { Link } from '@/lib/navigation';
+import { ArrowLeft } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import { Suspense } from 'react';
 
@@ -21,6 +24,12 @@ export default function StorageShoppingCartPage() {
       <Suspense fallback={<ShoppingCartTableSkeleton />}>
         <ShoppingCartTable messages={tableMessages} />
       </Suspense>
+      <Link href='/storage'>
+        <Button className='mx-auto flex gap-2'>
+          <ArrowLeft />
+          {t('backToStorage')}
+        </Button>
+      </Link>
     </>
   );
 }
