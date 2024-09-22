17afc70767d5ccfb20d6ac61d2a1c9c148225985
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-09-22 23:39:37 +0200

    chore: import useId alone like other hooks

--- src/components/news/ItemGridSkeleton.tsx
+++ src/components/news/ItemGridSkeleton.tsx
@@ -1,11 +1,11 @@
 import { ArticleItemSkeleton } from '@/components/news/ArticleItemSkeleton';
-import * as React from 'react';
+import { useId } from 'react';
 
 function ItemGridSkeleton() {
   return (
     <div className='grid min-h-[752px] grid-cols-1 gap-4 sm:min-h-[368px] sm:grid-cols-2 lg:min-h-[240px] lg:grid-cols-3'>
       {Array.from({ length: 6 }).map(() => (
-        <ArticleItemSkeleton key={React.useId()} />
+        <ArticleItemSkeleton key={useId()} />
       ))}
     </div>
   );
