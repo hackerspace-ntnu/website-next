665b665109023a60848d09b2cbce673e95259844
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-10-12 05:12:05 +0200

    chore: some fixes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,8 @@
 import { LogoLink } from '@/components/layout/LogoLink';
-import { MobileSheet } from '@/components/layout/MobileSheet';
 import { Nav } from '@/components/layout/Nav';
 import { DarkModeMenu } from '@/components/layout/header/DarkModeMenu';
 import { LocaleMenu } from '@/components/layout/header/LocaleMenu';
+import { MobileSheet } from '@/components/layout/header/MobileSheet';
 import { ProfileMenu } from '@/components/layout/header/ProfileMenu';
 import { useTranslations } from 'next-intl';
 
