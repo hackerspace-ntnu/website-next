3792169b7af1ce2e147f43b737f0c5de0caaa7ab
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-10-13 11:47:06 +0200

    chore: add main element to error pages

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Main } from '@/components/layout/Main';
 import { Button } from '@/components/ui/Button';
 import { Link } from '@/lib/locale/navigation';
 import { AlertTriangleIcon } from 'lucide-react';
@@ -18,7 +19,7 @@ export default function ErrorPage({
     console.error(error);
   }, [error]);
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center px-4 text-center'>
+    <Main className='flex min-h-svh flex-col items-center justify-center text-center'>
       <AlertTriangleIcon className='mb-6 xs:mb-8 size-16 xs:size-24 text-destructive' />
       <h1 className='mb-3 xs:mb-4 font-bold text-3xl xs:text-4xl'>
         {t('error')}
@@ -39,6 +40,6 @@ export default function ErrorPage({
           <Link href='/'>{t('goToHomepage')}</Link>
         </Button>
       </div>
-    </div>
+    </Main>
   );
 }
