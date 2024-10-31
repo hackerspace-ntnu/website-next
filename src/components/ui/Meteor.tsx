db6e20206f4f4e13b08ef996a002c3208c18682e
Supattra Wongsamal <supattrw@stud.ntnu.no>
2024-10-31 19:47:31 +0100

    fix: change from uuid to useId

--- src/components/ui/Meteor.tsx
+++ src/components/ui/Meteor.tsx
@@ -1,6 +1,6 @@
 import { cx } from '@/lib/utils';
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { useId } from 'react';
 
 export const Meteors = ({
   number,
@@ -11,7 +11,7 @@ export const Meteors = ({
   className?: string;
   hoverDelay?: string;
 }) => {
-  const meteors = new Array(number || 20).fill(null).map(() => uuidv4());
+  const meteors = new Array(number || 20).fill(null).map(() => useId());
 
   return (
     <>
@@ -19,8 +19,8 @@ export const Meteors = ({
         <span
           key={id} // Use unique ID as key
           className={cx(
-            'animate-meteor-effect absolute top-1/2 left-1/2 h-0.5 w-0.5 rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10] rotate-[215deg]',
-            'before:content-[""] before:absolute before:top-1/2 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-[#64748b] before:to-transparent',
+            'absolute top-1/2 left-1/2 h-0.5 w-0.5 rotate-[215deg] animate-meteor-effect rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]',
+            'before:-translate-y-[50%] before:absolute before:top-1/2 before:h-[1px] before:w-[50px] before:transform before:bg-gradient-to-r before:from-[#64748b] before:to-transparent before:content-[""]',
             className,
             'transition-all duration-1000',
           )}
