9b6367ced731cc1910a9d2a793f8b7e6f3c038a0
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2024-11-08 23:11:21 +0100

    fix: Correct grid cols on events loading

--- src/app/[locale]/(default)/events/loading.tsx
+++ src/app/[locale]/(default)/events/loading.tsx
@@ -7,14 +7,14 @@ export default function EventsSkeleton() {
       <h2 className='my-2'>Active events</h2>
       <EventCardSkeleton />
       <h2 className='my-4'>Upcoming events</h2>
-      <div className='grid grid-cols-2 gap-2'>
+      <div className='grid grid-cols-1 gap-2 lg:grid-cols-2'>
         <EventCardSkeleton />
         <EventCardSkeleton />
         <EventCardSkeleton />
         <EventCardSkeleton />
       </div>
       <h2 className='my-4'>Past events</h2>
-      <div className='grid grid-cols-2 gap-2'>
+      <div className='grid grid-cols-1 gap-2 lg:grid-cols-2'>
         <EventCardSkeleton />
         <EventCardSkeleton />
         <EventCardSkeleton />
