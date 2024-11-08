00861fdc47bfb23d4ff70e6366a6015e288961e9
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2024-11-08 23:41:45 +0100

    fix: Translate events loading

--- src/app/[locale]/(default)/events/loading.tsx
+++ src/app/[locale]/(default)/events/loading.tsx
@@ -1,19 +1,21 @@
 import { EventCardSkeleton } from '@/components/events/EventCardSkeleton';
+import { getTranslations } from 'next-intl/server';
 
-export default function EventsSkeleton() {
+export default async function EventsSkeleton() {
+  const t = await getTranslations('events');
   return (
     <>
-      <h1 className='my-4'>Events</h1>
-      <h2 className='my-2'>Active events</h2>
+      <h1 className='my-4'>{t('title')}</h1>
+      <h2 className='my-2'>{t('activeEvents')}</h2>
       <EventCardSkeleton />
-      <h2 className='my-4'>Upcoming events</h2>
+      <h2 className='my-4'>{t('upcomingEvents')}</h2>
       <div className='grid grid-cols-1 gap-2 lg:grid-cols-2'>
         <EventCardSkeleton />
         <EventCardSkeleton />
         <EventCardSkeleton />
         <EventCardSkeleton />
       </div>
-      <h2 className='my-4'>Past events</h2>
+      <h2 className='my-4'>{t('pastEvents')}</h2>
       <div className='grid grid-cols-1 gap-2 lg:grid-cols-2'>
         <EventCardSkeleton />
         <EventCardSkeleton />
