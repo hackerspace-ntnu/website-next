7243defcb594e397497f46d41b83e5ddfb103f01
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2024-11-08 23:27:37 +0100

    fix: Clean up css for event card skeletons, fix regressions on mobile

--- src/components/events/EventCardSkeleton.tsx
+++ src/components/events/EventCardSkeleton.tsx
@@ -14,13 +14,13 @@ function EventCardSkeleton() {
     <Card className='flex flex-col text-center'>
       <CardHeader>
         <Skeleton className='mx-auto h-8 w-3/4' />
-        <Skeleton className='mx-auto h-5 w-1/2' />
+        <Skeleton className='mx-auto h-4 w-1/2' />
       </CardHeader>
-      <CardContent className='flex flex-col-reverse items-center gap-8 md:flex-row md:justify-between'>
+      <CardContent className='flex flex-col-reverse items-center gap-2 md:flex-row md:justify-between'>
         <Skeleton className='h-16 w-full md:h-16 md:w-3/5 md:flex-1' />
-        <Skeleton className='h-36 w-36 rounded-full md:h-48 md:w-48' />
+        <Skeleton className='h-48 w-48 rounded-full' />
       </CardContent>
-      <CardFooter className='mt-auto flex-col gap-2'>
+      <CardFooter className='mt-auto flex-col gap-4'>
         <Skeleton className='h-5 w-32 md:w-56' />
         <Skeleton className='h-5 w-28 md:w-52' />
       </CardFooter>
