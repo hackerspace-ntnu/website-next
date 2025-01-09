f66af435e067c980d57bd7ad8227b4a24ce9c37d
Sander Eikemo Andreassen <seandreassen04@gmail.com>
2025-01-09 13:22:35 +0100

    feat: add variants and sizes to link component

--- src/components/composites/Link.tsx
+++ src/components/composites/Link.tsx
@@ -1,17 +1,15 @@
-import { Button, ButtonProps } from '@/components/ui/Button';
+import { Button, type buttonVariants } from '@/components/ui/Button';
 import { Link as InternalLink } from '@/lib/locale/navigation';
+import type { VariantProps } from 'cva';
 import ExternalLink from 'next/link';
-import { ComponentProps, type ComponentPropsWithoutRef } from 'react';
+import type { ComponentPropsWithoutRef } from 'react';
 
-type LinkProps = ComponentPropsWithoutRef<typeof InternalLink> & {
-  text: string;
-  variant?: string;
-  size?: string;
-};
+type LinkProps = ComponentPropsWithoutRef<typeof InternalLink> &
+  VariantProps<typeof buttonVariants>;
 
-function Link({ text, variant, size, children, ...props }: LinkProps) {
+function Link({ variant, size, children, ...props }: LinkProps) {
   return (
-    <Button asChild>
+    <Button variant={variant} size={size} asChild>
       <InternalLink {...props}>{children}</InternalLink>
     </Button>
   );
