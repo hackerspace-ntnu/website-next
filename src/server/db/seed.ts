d1910f062e2ab2c54930e3781753c92069d50128
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-01-09 16:57:09 +0100

    chore: update and fix tables

--- src/server/db/seed.ts
+++ src/server/db/seed.ts
@@ -2,98 +2,92 @@ import {
   type InsertSkill,
   type InsertUser,
   type InsertUserSkill,
-  locales,
   skills,
   users,
   usersSkills,
 } from '@/server/db/tables';
+import * as schema from '@/server/db/tables';
 import { fakerEN, fakerNB_NO, fakerSV } from '@faker-js/faker';
-
-import { routing } from '@/lib/locale';
+import { reset } from 'drizzle-seed';
 
 import { hashPassword } from '@/server/auth/password';
 import { db } from '@/server/db';
 
-const fakerMap = {
+const faker = {
   en: fakerEN,
   no: fakerNB_NO,
   sv: fakerSV,
 };
 
-console.log('Deleting existing data...');
-await db.delete(users);
-await db.delete(locales);
-console.log('Existing data deleted.');
+async function main() {
+  console.log('Resetting database...');
+  await reset(db, schema);
+  console.log('Database reset.');
 
-console.log('Inserting locales...');
-const insertedLocales = await db
-  .insert(locales)
-  .values(routing.locales.map((locale) => ({ locale })))
-  .returning();
-console.log('Locales inserted:', insertedLocales);
+  console.log('Inserting user...');
+  const user: InsertUser = {
+    name: 'Frank Sinatra',
+    username: 'fransin',
+    passwordHash: await hashPassword('Password1!'),
+  };
 
-console.log('Inserting user...');
-const user: InsertUser = {
-  name: 'Frank Sinatra',
-  username: 'fransin',
-  passwordHash: await hashPassword('Password1!'),
-};
+  const insertedUser = await db.insert(users).values(user).returning();
+  console.log('User inserted');
 
-const insertedUser = await db.insert(users).values(user).returning();
-console.log('User inserted');
+  console.log('Inserting skills...');
+  const skillsdata: InsertSkill[] = [
+    {
+      identifier: 'printing',
+    },
+    {
+      identifier: 'unix',
+    },
+    {
+      identifier: 'raspberry',
+    },
+    {
+      identifier: 'laser',
+    },
+    {
+      identifier: 'arduino',
+    },
+    {
+      identifier: 'souldering',
+    },
+    {
+      identifier: 'workshop',
+    },
+  ];
+  const insertedSkills = await db.insert(skills).values(skillsdata).returning();
+  console.log('Skills inserted');
 
-console.log('Inserting skills...');
-const skillsdata: InsertSkill[] = [
-  {
-    identifier: 'printing',
-  },
-  {
-    identifier: 'unix',
-  },
-  {
-    identifier: 'raspberry',
-  },
-  {
-    identifier: 'laser',
-  },
-  {
-    identifier: 'arduino',
-  },
-  {
-    identifier: 'souldering',
-  },
-  {
-    identifier: 'workshop',
-  },
-];
-const insertedSkills = await db.insert(skills).values(skillsdata).returning();
-console.log('Skills inserted');
+  if (insertedUser.length === 0 || insertedSkills.length < 5) {
+    console.error('Error: Inserted user or skills data is incomplete.');
+    process.exit(1);
+  }
 
-if (insertedUser.length === 0 || insertedSkills.length < 5) {
-  console.error('Error: Inserted user or skills data is incomplete.');
-  process.exit(1);
+  console.log('Inserting userskills...');
+  const usersSkillsData: InsertUserSkill[] = [
+    {
+      userId: insertedUser[0]?.id ?? 0,
+      skillId: insertedSkills[0]?.id ?? 0,
+    },
+    {
+      userId: insertedUser[0]?.id ?? 0,
+      skillId: insertedSkills[1]?.id ?? 0,
+    },
+    {
+      userId: insertedUser[0]?.id ?? 0,
+      skillId: insertedSkills[2]?.id ?? 0,
+    },
+    {
+      userId: insertedUser[0]?.id ?? 0,
+      skillId: insertedSkills[4]?.id ?? 0,
+    },
+  ];
+  await db.insert(usersSkills).values(usersSkillsData);
+  console.log('Userskills inserted');
 }
 
-console.log('Inserting userskills...');
-const usersSkillsData: InsertUserSkill[] = [
-  {
-    userId: insertedUser[0]?.id ?? 0,
-    skillId: insertedSkills[0]?.id ?? 0,
-  },
-  {
-    userId: insertedUser[0]?.id ?? 0,
-    skillId: insertedSkills[1]?.id ?? 0,
-  },
-  {
-    userId: insertedUser[0]?.id ?? 0,
-    skillId: insertedSkills[2]?.id ?? 0,
-  },
-  {
-    userId: insertedUser[0]?.id ?? 0,
-    skillId: insertedSkills[4]?.id ?? 0,
-  },
-];
-await db.insert(usersSkills).values(usersSkillsData);
-console.log('Userskills inserted');
-
+await main();
 process.exit();
--- src/server/db/tables/locales.ts
+++ src/server/db/tables/locales.ts
@@ -1,20 +1,7 @@
-import type { InferInsertModel, InferSelectModel } from 'drizzle-orm';
-import { index, pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+import { pgEnum } from 'drizzle-orm/pg-core';
 
-const locales = pgTable(
-  'locales',
-  {
-    id: serial('id').primaryKey(),
-    locale: varchar('locale', { length: 2 }).notNull(),
-  },
-  (table) => {
-    return {
-      localeIndex: index('locale_idx').on(table.locale),
-    };
-  },
-);
+import { routing } from '@/lib/locale';
 
-type SelectLocale = InferSelectModel<typeof locales>;
-type InsertLocale = InferInsertModel<typeof locales>;
+const locales = pgEnum('locale', routing.locales);
 
-export { locales, type SelectLocale, type InsertLocale };
+export { locales };
