b3b30a250bc14f787aa1e960e45753649540f6d5
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-01-23 06:46:20 +0100

    chore: migrate to bun s3

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import { env } from '@/env';
 import type { NextConfig } from 'next';
 import nextIntl from 'next-intl/plugin';
 
@@ -8,19 +7,6 @@ const config: NextConfig = {
   output: 'standalone',
   images: {
     formats: ['image/avif', 'image/webp'],
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/s3/:path*',
-          destination: `http://${env.S3_HOST}:${env.S3_PORT}/:path*`,
-          basePath: false,
-        },
-      ],
-      afterFiles: [],
-      fallback: [],
-    };
   },
 };
 
--- src/server/s3/index.ts
+++ src/server/s3/index.ts
@@ -1,5 +1,5 @@
 import { env } from '@/env';
-import { S3Client } from '@aws-sdk/client-s3';
+import { S3Client } from 'bun';
 
 const endpoint = `http://${env.S3_HOST}:${env.S3_PORT}`;
 
@@ -11,18 +11,15 @@ const globalForS3 = globalThis as unknown as {
 const s3 =
   globalForS3.s3 ??
   new S3Client({
-    credentials: {
-      accessKeyId: env.S3_USER,
-      secretAccessKey: env.S3_PASSWORD,
-    },
-    endpoint: endpoint,
-    forcePathStyle: true,
+    accessKeyId: env.S3_USER,
+    secretAccessKey: env.S3_PASSWORD,
+    endpoint,
+    bucket: env.S3_NAME,
     region: 'auto', // Required but not used with self-hosted storage
   });
 
 if (env.NODE_ENV !== 'production') globalForS3.s3 = s3;
 
-const buckets = env.S3_BUCKETS.split(',');
-const imageBucket = buckets[0];
+Bun.s3 = s3;
 
-export { s3, endpoint, imageBucket };
+export { s3, endpoint };
