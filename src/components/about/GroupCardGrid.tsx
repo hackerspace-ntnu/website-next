b25f126eceba64ea45e0376789f468b29bcb5c76
lisay <lisay@stud.ntnu.no>
2025-02-03 15:23:26 +0100

    fix: change design

--- src/components/about/GroupCardGrid.tsx
+++ src/components/about/GroupCardGrid.tsx
@@ -3,7 +3,7 @@ import { GroupCard } from './GroupCard';
 
 function GroupCardGrid() {
   return (
-    <div className='mx-auto grid w-11/12 justify-items-center gap-y-1 sm:grid-cols-3 sm:gap-24 lg:grid-cols-3'>
+    <div className='mx-auto grid w-11/12 justify-items-center sm:grid-cols-3 sm:gap-x-24 sm:gap-y-7 lg:grid-cols-3'>
       {groupData.map((data) => (
         <GroupCard
           key={data.id}
