2a8d435f1610b094161928b04ae528d35394f83a
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-07 14:05:01 +0100

    feat: generated migrations

--- src/app/[locale]/auth/verify-email/page.tsx
+++ src/app/[locale]/auth/verify-email/page.tsx
@@ -17,5 +17,5 @@ export default async function VerifyEmailPage({
     return redirect({ href: '/auth', locale });
   }
 
-  return <VerifyEmailForm email={user.email} />;
+  return <VerifyEmailForm />;
 }
