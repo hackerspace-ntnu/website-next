504d0dc71fd4252ce34427f66a934961480e73f6
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-17 20:49:03 +0100

    chore: remove config file again

--- src/lib/styles/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'tailwindcss';
-import tailwindClampPlugin from './clampPlugin';
-
-const config: Config = {
-  plugins: [tailwindClampPlugin],
-};
-
-export default config;
