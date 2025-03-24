9760e177f3d9367c0d403fa5a02c8677102c89d2
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2025-03-24 01:55:03 +0100

    feat: Lock storage item edit page from unauthorized users

--- src/app/[locale]/(default)/storage/item/[id]/edit/page.tsx
+++ src/app/[locale]/(default)/storage/item/[id]/edit/page.tsx
@@ -1,3 +1,4 @@
+import ErrorPage from '@/app/[locale]/error';
 import { EditItemForm } from '@/components/storage/EditItemForm';
 import { api } from '@/lib/api/server';
 import { NextIntlClientProvider } from 'next-intl';
@@ -6,6 +7,7 @@ import {
   getTranslations,
   setRequestLocale,
 } from 'next-intl/server';
+import { NextResponse } from 'next/server';
 
 export async function generateMetadata() {
   const t = await getTranslations('storage');
@@ -30,6 +32,13 @@ export default async function EditStorageItemPage({
 
   const item = await api.storage.fetchOne(Number(id));
   const itemCategories = await api.storage.fetchItemCategoryNames();
+
+  const auth = await api.auth.state();
+
+  if (!auth.user?.groups.some((g) => ["labops", "leadership", "admin"].includes(g))) {
+    // TODO: Actually return a HTTP 401 Unauthorized reponse whenever `unathorized.tsx` is stable
+    throw new Error(t('unauthorized'));
+  }
 
   return (
     <div className='mx-auto max-w-prose space-y-8'>
