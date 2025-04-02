edd6713e98c0b4f625bb4b35f61caa01f06b8f99
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-04-02 16:16:48 +0200

    refractor: change any fluid tailwind css classes and fix styling in toolcard

--- src/components/reservations/ToolCard.tsx
+++ src/components/reservations/ToolCard.tsx
@@ -29,17 +29,18 @@ export function ToolCard({ tool, onClick }: ToolCardProps) {
   const id = useId();
 
   return (
-    <Card className='relative z-0 flex h-112 w-80 flex-col overflow-hidden rounded-xl hover:brightness-110'>
+    <Card className='relative z-0 flex h-112 w-80 flex-col overflow-hidden rounded-xl hover:brightness-105'>
       <motion.div
         layoutId={`${tool.type}-${tool.title}-${id}`}
         className='z-0 flex h-full flex-col'
       >
         <div
-          className='flex max-h-56 items-center justify-center hover:brightness-110'
+          className='flex max-h-56 items-center justify-center'
           onMouseEnter={() => setHovered(true)}
           onMouseLeave={() => setHovered(false)}
         >
           <Image
+            priority
             width={185}
             height={185}
             src={tool.photoUrl}
@@ -47,31 +48,35 @@ export function ToolCard({ tool, onClick }: ToolCardProps) {
             className='h-48 w-full rounded-t-g object-fill'
           />
           <div className='z-0'>
-            <motion.button
-              className='absolute top-2 right-2 z-10 inline-flex size-11 cursor-pointer items-center justify-center rounded-full bg-stone-500 bg-opacity-50 backdrop-blur-sm ease-in-out hover:bg-primary'
-              key={`cardHeaderButton-${tool.title}-${id}`}
-              onClick={onClick}
-              animate={{ scale: hovered ? 1.1 : 1 }}
-            >
-              <Maximize2 className='size-6 transform stroke-stone-300 transition delay-75 duration-300 ease-in-out hover:scale-110' />
-            </motion.button>
-            {hovered && (
-              <motion.div
-                key={`cardHeaderToolTip-${tool.title}-${id}`}
-                initial={{ opacity: 0, x: 20 }}
-                animate={{ opacity: 1, x: -4 }}
-                transition={{ type: 'tween', delay: 0.2 }}
-                className='~text-xs/sm absolute top-3 right-11 z-0 whitespace-nowrap rounded-xl rounded-r-none bg-black bg-opacity-40 px-3 py-2 pr-4 text-white shadow-lg'
+            <AnimatePresence>
+              <motion.button
+                className='absolute top-2 right-2 z-10 inline-flex size-11 cursor-pointer items-center justify-center rounded-full bg-stone-500 bg-opacity-50 backdrop-blur-sm ease-in-out hover:bg-primary'
+                key={`cardHeaderButton-${tool.title}-${id}`}
+                onClick={onClick}
+                animate={{ scale: hovered ? 1.1 : 1 }}
               >
-                {t('tools.tooltip')}
-              </motion.div>
-            )}
+                <Maximize2 className='size-6 transform stroke-stone-300 transition delay-75 duration-300 ease-in-out hover:scale-110' />
+              </motion.button>
+              {hovered && (
+                <motion.div
+                  key={`cardHeaderToolTip-${tool.title}-${id}`}
+                  initial={{ opacity: 0, x: 20 }}
+                  animate={{ opacity: 1, x: 10 }}
+                  transition={{ type: 'tween', delay: 0.2 }}
+                  className='absolute top-3 right-11 z-0 whitespace-nowrap rounded-xl rounded-r-none bg-popover py-2 pr-6 pl-3 text-xs-sm-clamp shadow-lg'
+                >
+                  {t('tools.tooltip')}
+                </motion.div>
+              )}
+            </AnimatePresence>
           </div>
         </div>
-        <div className='mt-2 flex h-full flex-col gap-2'>
-          <h1 className='text-wrap px-1 text-center text-xl'>{tool.title}</h1>
-          <h2 className='~text-base/lg text-center'>{tool.description}</h2>
-          <div className='relative max-h-fit text-wrap pl-8 text-left text-sm'>
+        <div className='mt-2 flex h-full flex-col gap-1 px-5 text-left'>
+          <h1 className='text-wrap text-center text-lg-xl-clamp'>
+            {tool.title}
+          </h1>
+          <h2 className='text-sm-lg-clamp'>{tool.description}</h2>
+          <div className='relative line-clamp-6 max-h-fit text-clip-3 text-xs-sm-clamp'>
             {tool.type === 'printer' &&
               fieldsToShow.map((field) => {
                 const text = tool[field];
@@ -79,7 +84,7 @@ export function ToolCard({ tool, onClick }: ToolCardProps) {
                   return null;
                 return (
                   <p key={field}>
-                    {field}: {text}
+                    {field.charAt(0).toUpperCase() + field.slice(1)}: {text}
                     <br />
                   </p>
                 );
@@ -94,22 +99,25 @@ export function ToolCard({ tool, onClick }: ToolCardProps) {
                 }}
                 className='absolute bottom-0 left-0 h-14 w-full'
               >
-                <Button className='h-full w-full rounded-xl rounded-t-none hover:brightness-125'>
+                <Button className='size-full rounded-t-none text-base-lg-clamp hover:brightness-110'>
                   {t('tools.available')}
                 </Button>
               </Link>
             ) : (
               <Button
                 variant='destructive'
-                className='pointer-events-none absolute bottom-0 left-0 h-14 w-full rounded-xl rounded-t-none'
+                className='pointer-events-none absolute bottom-0 left-0 h-14 w-full rounded-t-none text-base-lg-clamp'
               >
                 {t('tools.unavailable')}
               </Button>
             )
           ) : (
-            <div className='absolute bottom-0 left-0 flex h-14 w-full items-center justify-center rounded-xl rounded-t-none bg-gray-600 bg-opacity-50 text-center'>
+            <Button
+              variant='secondary'
+              className='pointer-events-none absolute bottom-0 left-0 flex h-14 w-full rounded-t-none text-center text-base-lg-clamp brightness-90'
+            >
               {t('tools.supervision')}
-            </div>
+            </Button>
           )}
         </div>
       </motion.div>
