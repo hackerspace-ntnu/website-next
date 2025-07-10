a10b708e5774abc241c2c142653c2d59ff40b122
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2025-07-11 00:11:15 +0200

    feat: Change internal badge render and position

--- src/components/members/InternalBadge.tsx
+++ src/components/members/InternalBadge.tsx
@@ -13,12 +13,11 @@ import {
 
 type InternalBadgeProps = {
   className?: string;
-  internal: boolean;
 };
 
-function InternalBadge({ className, internal }: InternalBadgeProps) {
+function InternalBadge({ className }: InternalBadgeProps) {
   const t = useTranslations('members');
-  if (!internal) return null;
+
   return (
     <TooltipProvider>
       <Tooltip>
