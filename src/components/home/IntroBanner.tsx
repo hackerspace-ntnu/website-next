3bd9951dc93bc726ef4ff7c1509469ce10a726a4
Sander Eikemo Andreassen <seandreassen04@gmail.com>
2025-07-13 19:06:42 +0000

    feat: add image carousel

--- src/components/home/IntroBanner.tsx
+++ src/components/home/IntroBanner.tsx
@@ -1,24 +1,47 @@
-import Image from 'next/image';
+'use client';
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from '@/components/ui/Carousel';
+import Autoplay from 'embla-carousel-autoplay';
 
 function IntroBanner() {
+  const images = [
+    <img key={1} src='/bg.jpg' alt='...' />,
+    <img key={2} src='/mock.jpg' alt='...' />,
+    <img key={3} src='/bg.jpg' alt='...' />,
+    <img key={4} src='/event.webp' alt='...' />,
+  ];
+
   return (
-    <div className='h-[calc(100vh-5rem)]'>
-      <Image
-        className='mx-auto object-cover'
-        src='/bg.jpg'
-        alt='...'
-        fill
-        loading='eager'
-      />
-      <div className='-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 flex w-full transform flex-col gap-5 text-center text-background shadow-foreground dark:text-foreground dark:shadow-background'>
-        <h1 className='~sm/2xl:~text-4xl/7xl [text-shadow:_6px_3px_4px_var(--tw-shadow-color)]'>
+    <>
+      <div className='absolute top-0 left-0'>
+        <Carousel
+          opts={{
+            loop: true,
+          }}
+          plugins={[
+            Autoplay({
+              delay: 10000,
+            }),
+          ]}
+        >
+          <CarouselContent className='ml-0 h-screen w-screen'>
+            {images.map((image) => (
+              <CarouselItem key={image.key} className='h-screen w-screen pl-0'>
+                {image}
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+        <h1 className='-translate-x-1/2 -translate-y-1/2 ~sm/2xl:~text-4xl/7xl absolute top-5/12 left-1/2 w-full transform gap-5 text-center text-background text-shadow-foreground text-shadow-md dark:text-foreground dark:text-shadow-background'>
           Hackerspace NTNU
         </h1>
-        <span className='~sm/2xl:~text-lg/2xl [text-shadow:_4px_2px_2.67px_var(--tw-shadow-color)]'>
-          noe spennende tekst her nede
-        </span>
       </div>
-    </div>
+      <div className='h-[calc(100vh-5rem)]' />
+    </>
   );
 }
 
