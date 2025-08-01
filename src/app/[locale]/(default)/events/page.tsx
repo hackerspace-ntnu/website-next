978f8bb0fbbfbe725a50fa489c6269f27bb3a5c0
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2025-08-02 00:32:17 +0200

    fix: Fetch internal events only for members

--- src/app/[locale]/(default)/events/page.tsx
+++ src/app/[locale]/(default)/events/page.tsx
@@ -1,8 +1,14 @@
 import { and, asc, desc, eq, gte, lte, notInArray } from 'drizzle-orm';
 import type { Locale } from 'next-intl';
 import { getTranslations, setRequestLocale } from 'next-intl/server';
+import {
+  createSearchParamsCache,
+  parseAsInteger,
+  type SearchParams,
+} from 'nuqs/server';
 import { EventCard } from '@/components/events/EventCard';
 import { ExternalLink } from '@/components/ui/Link';
+import { api } from '@/lib/api/server';
 import { db } from '@/server/db';
 import { eventLocalizations, events as eventsTable } from '@/server/db/tables';
 
@@ -14,10 +20,14 @@ export async function generateMetadata() {
   };
 }
 
+const ITEMS_PER_PAGE = 10;
+
 export default async function EventsPage({
   params,
+  searchParams,
 }: {
   params: Promise<{ locale: Locale }>;
+  searchParams: Promise<SearchParams>;
 }) {
   const { locale } = await params;
   setRequestLocale(locale);
@@ -25,11 +35,24 @@ export default async function EventsPage({
   const tLayout = await getTranslations('layout');
   const tUi = await getTranslations('ui');
 
+  const searchParamsCache = createSearchParamsCache({
+    [tUi('page')]: parseAsInteger.withDefault(1),
+  });
+
+  const { [tUi('page')]: page } = searchParamsCache.parse(await searchParams);
+
+  const { user } = await api.auth.state();
+
+  const activeEventsWhere = [
+    lte(eventsTable.startTime, new Date()),
+    gte(eventsTable.endTime, new Date()),
+  ];
+
   const activeEvents = await db.query.events.findMany({
-    where: and(
-      lte(eventsTable.startTime, new Date()),
-      gte(eventsTable.endTime, new Date()),
-    ),
+    where:
+      user && user?.groups.length > 0
+        ? and(...activeEventsWhere)
+        : and(...activeEventsWhere, eq(eventsTable.internal, false)),
     orderBy: asc(eventsTable.startTime),
     with: {
       localizations: {
@@ -38,19 +61,24 @@ export default async function EventsPage({
     },
   });
 
+  const rawEventsWhere = notInArray(
+    eventsTable.id,
+    activeEvents.map((e) => e.id),
+  );
+
   const rawEvents = await db.query.events.findMany({
-    where: notInArray(
-      eventsTable.id,
-      activeEvents.map((e) => e.id),
-    ),
+    where:
+      user && user.groups.length > 0
+        ? rawEventsWhere
+        : and(rawEventsWhere, eq(eventsTable.internal, false)),
     orderBy: desc(eventsTable.startTime),
     with: {
       localizations: {
         where: eq(eventLocalizations.locale, locale),
       },
     },
-    limit: 10,
-    offset: 0,
+    limit: ITEMS_PER_PAGE,
+    offset: ((page as number) - 1) * ITEMS_PER_PAGE,
   });
 
   const events = rawEvents.filter((event) => event.localizations.length === 1);
