7fa62ebd96a6b1aa1f776005e84b04b28c4bd8a3
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2025-08-06 15:14:44 +0200

    fix: Clamp on main

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -7,7 +7,7 @@ function Main({
   return (
     <main
       className={cx(
-        'clamp-[px-4-24-clamp py-6] mx-auto w-screen max-w-screen-2xl grow',
+        'clamp-[px-4-24-clamp] mx-auto w-screen max-w-screen-2xl grow py-6',
         className,
       )}
       {...props}
