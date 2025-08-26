227abe8e837ef008026335d69c0a71be77cd1e80
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-08-26 17:01:33 +0200

    refractor: remove unused fragment

--- src/app/[locale]/(default)/reservations/[Id]/page.tsx
+++ src/app/[locale]/(default)/reservations/[Id]/page.tsx
@@ -1,9 +1,5 @@
 import ToolCalendar from '@/components/reservations/ReservationsCalendar/ToolCalendar';
 
 export default async function ToolCalendarPage() {
-  return (
-    <>
-      <ToolCalendar />
-    </>
-  );
+  return <ToolCalendar />;
 }
