862530377970f7a21e0acef53ed4741cf1a6086a
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2025-08-27 20:31:18 +0200

    feat: Optimistic UI for event attendance checkbox

--- src/components/events/AttendanceCheckbox.tsx
+++ src/components/events/AttendanceCheckbox.tsx
@@ -3,7 +3,6 @@
 import { useRouter } from 'next/navigation';
 import { useTranslations } from 'next-intl';
 import { Checkbox } from '@/components/ui/Checkbox';
-import { Label } from '@/components/ui/Label';
 import { toast } from '@/components/ui/Toaster';
 import { api } from '@/lib/api/client';
 import type { RouterOutput } from '@/server/api';
@@ -19,44 +18,44 @@ function AttendanceCheckbox({
   const router = useRouter();
   const setParticipantAttendance =
     api.events.setParticipantAttendance.useMutation({
+      onError: () => {
+        router.refresh();
+      },
       onSuccess: () => {
         router.refresh();
       },
     });
 
   return (
-    <>
-      <Checkbox
-        checked={participant.attended}
-        className='cursor-pointer'
-        onClick={() => {
-          if (event.startTime > new Date()) {
-            return toast.error(t('notStartedYet'));
-          }
-          toast.promise(
-            setParticipantAttendance.mutateAsync({
-              eventId: event.id,
-              userId: participant.userId,
-              attended: !participant.attended,
+    <Checkbox
+      checked={
+        setParticipantAttendance.variables?.attended ?? participant.attended
+      }
+      className='mx-auto cursor-pointer'
+      onClick={() => {
+        if (event.startTime > new Date()) {
+          return toast.error(t('notStartedYet'));
+        }
+        toast.promise(
+          setParticipantAttendance.mutateAsync({
+            eventId: event.id,
+            userId: participant.userId,
+            attended: !participant.attended,
+          }),
+          {
+            loading: t('updating', {
+              name: `${participant.user.firstName} ${participant.user.lastName}`,
             }),
-            {
-              loading: t('updating', {
-                name: `${participant.user.firstName} ${participant.user.lastName}`,
-              }),
-              success: t('success', {
-                name: `${participant.user.firstName} ${participant.user.lastName}`,
-              }),
-              error: t('error', {
-                name: `${participant.user.firstName} ${participant.user.lastName}`,
-              }),
-            },
-          );
-        }}
-      />
-      <Label className='whitespace-nowrap'>
-        {participant.attended ? t('present') : t('absent')}
-      </Label>
-    </>
+            success: t('success', {
+              name: `${participant.user.firstName} ${participant.user.lastName}`,
+            }),
+            error: t('error', {
+              name: `${participant.user.firstName} ${participant.user.lastName}`,
+            }),
+          },
+        );
+      }}
+    />
   );
 }
 
