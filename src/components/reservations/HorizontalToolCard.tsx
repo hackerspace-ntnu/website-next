7a7874d57813d4457761b7dac9f421c486ad7eed
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-09-08 15:55:34 +0200

    refractor: fix classes in horizontaltoolcalrd

--- src/components/reservations/HorizontalToolCard.tsx
+++ src/components/reservations/HorizontalToolCard.tsx
@@ -7,7 +7,7 @@ import { useTranslations } from 'next-intl';
 import { useId } from 'react';
 import type { Tool } from '@/components/reservations/ToolCardGrid';
 import { Button } from '@/components/ui/Button';
-import { Card, CardContent, CardTitle } from '@/components/ui/Card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Card';
 import { Link } from '@/lib/locale/navigation';
 
 type HorizontalToolCardProps = {
@@ -21,61 +21,56 @@ function HorizontalToolCard({ tool, onClick }: HorizontalToolCardProps) {
 
   return (
     <Card className='flex h-30 w-full overflow-hidden rounded-xl hover:brightness-105'>
-      <m.div
-        layoutId={`${tool.type}-${tool.title}-${id}`}
-        className='flex h-full w-full flex-row'
-      >
-        <div className='relative h-full w-40 flex-shrink-0'>
-          <Image
-            src={tool.photoUrl}
-            alt={tool.title}
-            fill
-            className='object-cover'
-          />
-        </div>
-        <CardContent className='relative flex h-full w-full flex-col items-center justify-center gap-1 text-wrap px-2 text-center'>
-          <CardTitle className='line-clamp-1 text-lg'>{tool.title}</CardTitle>
-          <m.button
-            title={t('tools.tooltip')}
-            className='absolute top-1 right-1 z-10 mx-auto inline-flex size-9 cursor-pointer items-center justify-center rounded-full bg-stone-500 bg-opacity-50 backdrop-blur-sm ease-in-out hover:bg-primary'
-            key={`cardHeaderButton-${tool.title}-${id}`}
-            onClick={onClick}
-          >
-            <Maximize2Icon className='size-6 transform stroke-stone-300 transition delay-75 duration-300 ease-in-out hover:scale-110' />
-          </m.button>
-          <div className='absolute bottom-0 left-0 w-full'>
-            {tool.type === 'printer' ? (
-              tool.available ? (
-                <Link
-                  href={{
-                    pathname: '/reservations/[reservationId]',
-                    params: { reservationId: tool.toolId },
-                  }}
-                  className='w-full hover:brightness-110'
-                >
-                  <Button className='w-full rounded-none'>
-                    {t('tools.available')}
-                  </Button>
-                </Link>
-              ) : (
-                <Button
-                  variant='destructive'
-                  className='pointer-events-none w-full rounded-none '
-                >
-                  {t('tools.unavailable')}
+      <CardHeader className='relative h-full w-40 flex-shrink-0'>
+        <Image
+          src={tool.photoUrl}
+          alt={tool.title}
+          fill
+          className='object-cover'
+        />
+      </CardHeader>
+      <CardContent className='relative flex h-full w-full flex-col items-center justify-center p-0 text-center'>
+        <m.button
+          title={t('tools.tooltip')}
+          className='absolute top-1 right-1 z-10 mx-auto inline-flex size-9 cursor-pointer items-center justify-center rounded-full bg-stone-500 bg-opacity-50 backdrop-blur-sm ease-in-out hover:bg-primary'
+          key={`cardHeaderButton-${tool.title}-${id}`}
+          onClick={onClick}
+        >
+          <Maximize2Icon className='size-6 transform stroke-stone-300 transition delay-75 duration-300 ease-in-out hover:scale-110' />
+        </m.button>
+        <CardTitle className='line-clamp-1 text-lg'>{tool.title}</CardTitle>
+        <div className='absolute bottom-0 left-0 w-full'>
+          {tool.type === 'printer' ? (
+            tool.available ? (
+              <Link
+                href={{
+                  pathname: '/reservations/[reservationId]',
+                  params: { reservationId: tool.toolId },
+                }}
+                className='w-full hover:brightness-110'
+              >
+                <Button className='w-full rounded-none'>
+                  {t('tools.available')}
                 </Button>
-              )
+              </Link>
             ) : (
               <Button
-                variant='secondary'
-                className='pointer-events-none w-full rounded-none brightness-90 '
+                variant='destructive'
+                className='pointer-events-none w-full rounded-none '
               >
-                {t('tools.supervision')}
+                {t('tools.unavailable')}
               </Button>
-            )}
-          </div>
-        </CardContent>
-      </m.div>
+            )
+          ) : (
+            <Button
+              variant='secondary'
+              className='pointer-events-none w-full rounded-none brightness-90 '
+            >
+              {t('tools.supervision')}
+            </Button>
+          )}
+        </div>
+      </CardContent>
     </Card>
   );
 }
