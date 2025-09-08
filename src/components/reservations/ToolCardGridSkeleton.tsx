c3eaa52f75842a2ab48879c91011b6f5137994db
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-09-08 15:59:07 +0200

    refractor: remove unnecessary useeffect

--- src/components/reservations/ToolCardGridSkeleton.tsx
+++ src/components/reservations/ToolCardGridSkeleton.tsx
@@ -8,21 +8,12 @@ import { useMediaQuery } from '@/lib/hooks/useMediaQuery';
 export function ToolCardGridSkeleton() {
   const list = [useId(), useId(), useId(), useId(), useId(), useId()];
   const isDesktop = useMediaQuery('(min-width: 45.4rem)');
-  const [smallScreen, setSmallScreen] = useState(false);
-
-  useEffect(() => {
-    if (!isDesktop) {
-      setSmallScreen(true);
-    } else {
-      setSmallScreen(false);
-    }
-  }, [isDesktop]);
 
   return (
     <div className='size-full'>
       <ul className='mx-auto flex w-full max-w-5xl flex-wrap items-center justify-center gap-4'>
         {list.map((id) =>
-          smallScreen ? (
+          !isDesktop ? (
             <HorizontalToolCardSkeleton key={id} />
           ) : (
             <ToolCardSkeleton key={id} />
