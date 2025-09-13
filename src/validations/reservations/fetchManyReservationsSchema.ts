5f851fe5d266c53534f1e592dc87bd58ec0bb169
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-09-14 00:58:00 +0200

    refactor: correct fetchManyReservationscSchema

--- src/validations/reservations/fetchManyReservationsSchema.ts
+++ src/validations/reservations/fetchManyReservationsSchema.ts
@@ -3,18 +3,7 @@ import { z } from 'zod';
 import type { Translations } from '@/lib/locale';
 
 function fetchManyReservationsSchema(t: Translations) {
-  return z
-    .object({
-      toolId: z.number(),
-      range: z.object({
-        from: z.date(),
-        to: z.date(),
-      }),
-    })
-    .refine(
-      (data) => isBefore(data.range.to, data.range.from),
-      t('reservations.api.startBeforeEndError'),
-    );
+  return z.number().min(1, t('reservations.api.invalidId')).max(50);
 }
 
 export { fetchManyReservationsSchema };
