a1d952f2b46c416d2df8946800fda9bbaccbbd3e
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-09-19 14:53:28 +0200

    refactor: change reservation data in seed

---
+++ src/validations/reservations/index.ts
@@ -0,0 +1,3 @@
+export * from '@/validations/reservations/createReservationSchema';
+export * from '@/validations/reservations/deleteReservationSchema';
+export * from '@/validations/reservations/fetchManyReservationsSchema';
