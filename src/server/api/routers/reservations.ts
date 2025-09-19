be69089b1db893cba625ee6714a2a52d7f328296
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-09-19 14:52:37 +0200

    feat: add procedures for fetching one, many and deleting a reservation in reservations router

--- src/server/api/routers/reservations.ts
+++ src/server/api/routers/reservations.ts
@@ -1,5 +1,93 @@
+import { TRPCError } from '@trpc/server';
+import { and, asc, eq } from 'drizzle-orm';
+import { useTranslationsFromContext } from '@/server/api/locale';
+import {
+  authenticatedProcedure,
+  protectedProcedure,
+  publicProcedure,
+} from '@/server/api/procedures';
 import { createRouter } from '@/server/api/trpc';
+import {
+  toolReservations,
+  tools,
+  toolsLocalizations,
+  users,
+} from '@/server/db/tables';
+import {
+  deleteReservationSchema,
+  fetchManyReservationsSchema,
+} from '@/validations/reservations';
+import { fetchOneReservationSchema } from '@/validations/reservations/fetchOneReservationSchema';
 
-const reservationsRouter = createRouter({});
+const reservationsRouter = createRouter({
+  fetchOne: publicProcedure
+    .input((input) =>
+      fetchOneReservationSchema(useTranslationsFromContext()).parse(input),
+    )
+    .query(async ({ ctx, input }) => {
+      const reservation = await ctx.db
+        .select({
+          reservation: toolReservations,
+        })
+        .from(toolReservations)
+        .where(eq(toolReservations.id, input));
+
+      if (!reservation) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: ctx.t('reservations.api.notFound'),
+          cause: { toast: 'error' },
+        });
+      }
+      return reservation;
+    }),
+
+  fetchUserReservations: authenticatedProcedure.query(async ({ ctx }) => {
+    const userReservations = await ctx.db
+      .select({
+        reservation: toolReservations,
+        toolSlug: tools.slug,
+        toolName: toolsLocalizations.name,
+      })
+      .from(toolReservations)
+      .where(
+        and(
+          eq(toolReservations.reservorId, ctx.user.id),
+          eq(toolReservations.finished, false),
+          eq(toolsLocalizations.locale, ctx.locale),
+        ),
+      )
+      .innerJoin(tools, eq(tools.id, toolReservations.toolId))
+      .innerJoin(
+        toolsLocalizations,
+        eq(toolsLocalizations.toolId, toolReservations.toolId),
+      )
+      .orderBy(asc(toolReservations.reservedFrom))
+      .catch((error) => {
+        console.error(error);
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: '',
+          cause: { toast: error },
+        });
+      });
+
+    return userReservations;
+  }),
+
+  deleteReservation: protectedProcedure
+    .input((input) => deleteReservationSchema().parse(input))
+    .mutation(async ({ input, ctx }) => {
+      await ctx.db
+        .delete(toolReservations)
+        .where(
+          and(
+            eq(toolReservations.id, input.reservationId),
+            eq(toolReservations.toolId, input.toolId),
+            eq(toolReservations.reservorId, input.reservorId),
+          ),
+        );
+    }),
+});
 
 export { reservationsRouter };
