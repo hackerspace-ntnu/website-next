2765079ada674431761c43da48f0b944754f4b33
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-09-23 16:23:31 +0200

    refactor: add translations for schemas and procedures, for reservations

--- src/validations/reservations/index.ts
+++ src/validations/reservations/index.ts
@@ -2,4 +2,6 @@ export * from '@/validations/reservations/createReservationSchema';
 export * from '@/validations/reservations/deleteReservationSchema';
 export * from '@/validations/reservations/fetchCalendarReservations';
 export * from '@/validations/reservations/fetchManyReservationsSchema';
+export * from '@/validations/reservations/fetchOneReservationSchema';
 export * from '@/validations/reservations/fetchToolSchema';
+export * from '@/validations/reservations/updateReservationSchema';
