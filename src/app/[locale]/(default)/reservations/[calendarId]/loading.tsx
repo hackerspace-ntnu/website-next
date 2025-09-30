09904fb3347f852c1b5abe7cf2c59ca5cf68bfc3
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-09-30 15:27:18 +0200

    refactor: move toolcalendar skeletong to its own component

--- src/app/[locale]/(default)/reservations/[calendarId]/loading.tsx
+++ src/app/[locale]/(default)/reservations/[calendarId]/loading.tsx
@@ -1,17 +1,5 @@
-import { Card } from '@/components/ui/Card';
-import { Skeleton } from '@/components/ui/Skeleton';
+import { ToolCalendarSkeleton } from '@/components/reservations/reservations-calendar/ToolCalendarSkeleton';
 
 export default function CalendarLoading() {
-  return (
-    <div className='flex h-full w-full flex-col gap-2'>
-      <Skeleton className='h-[40px] w-[184px] self-center rounded-xl' />
-      <Card className='flex h-[847.75px] w-[1432.5px] flex-col gap-2 overflow-hidden rounded-xl p-1'>
-        <Skeleton className='h-[109.5px] w-full rounded-xl' />
-
-        <Skeleton className='mx-auto h-full w-full rounded-xl' />
-
-        <Skeleton className='mt-auto h-[142px] w-full rounded-xl' />
-      </Card>
-    </div>
-  );
+  return <ToolCalendarSkeleton />;
 }
---
+++ src/components/reservations/reservations-calendar/ToolCalendarSkeleton.tsx
@@ -0,0 +1,19 @@
+import { Card } from '@/components/ui/Card';
+import { Skeleton } from '@/components/ui/Skeleton';
+
+function ToolCalendarSkeleton() {
+  return (
+    <div className='flex h-full w-full flex-col gap-2'>
+      <Skeleton className='h-[40px] w-[184px] self-center rounded-xl' />
+      <Card className='flex h-[1044px] w-full max-w-[1432.5px] flex-col gap-2 overflow-hidden rounded-xl p-1'>
+        <Skeleton className='h-[109.5px] w-full rounded-xl' />
+
+        <Skeleton className='mt-auto h-full max-h-[142px] w-full rounded-xl' />
+
+        <Skeleton className='mx-auto h-full max-h-[792.5px] w-full rounded-xl' />
+      </Card>
+    </div>
+  );
+}
+
+export { ToolCalendarSkeleton };
