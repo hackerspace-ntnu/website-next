97047cadfc4581ce56a1d8b0f2c02376e444a6b4
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-10-01 00:18:59 +0200

    refactor: removed styling for resizing in customEventContent component

--- src/components/reservations/reservations-calendar/CustomEventContent.tsx
+++ src/components/reservations/reservations-calendar/CustomEventContent.tsx
@@ -4,16 +4,10 @@ import { Separator } from '@/components/ui/Separator'; */
 import { useFormatter } from 'next-intl';
 
 type CustomEventStylingProps = {
-  /*   isLoggedIn: boolean;
-  userId: number; */
   eventInfo: EventContentArg;
 };
 
-function CustomEventContent({
-  /*   isLoggedIn,
-  userId, */
-  eventInfo,
-}: CustomEventStylingProps) {
+function CustomEventContent({ eventInfo }: CustomEventStylingProps) {
   const format = useFormatter();
 
   const sameDay =
@@ -58,24 +52,11 @@ function CustomEventContent({
 
   return (
     <div className='flex h-full flex-col items-center justify-between overflow-hidden'>
-      {/*       {isLoggedIn && userId === res.event.extendedProps.userId && (
-        <div className='flex w-full flex-col'>
-          <ArrowUpToLineIcon className='clamp-[size-base-xs-clamp] self-center' />
-          <Separator className='mb-1 w-full dark:bg-foreground' />
-        </div>
-      )} */}
       <div className='clamp-[text-lg-sm-clamp] flex size-full flex-col items-center justify-between gap-64 overflow-hidden px-1 py-2 font-semibold'>
         {infoBlock}
         {durationHours >= 6 && infoBlock}
         {durationHours >= 12 && infoBlock}
       </div>
-
-      {/*       {isLoggedIn && userId === res.event.extendedProps.userId && (
-        <div className='flex w-full flex-col'>
-          <Separator className='mt-1 w-full dark:bg-foreground' />
-          <ArrowDownToLineIcon className='self-center' />
-        </div>
-      )} */}
     </div>
   );
 }
