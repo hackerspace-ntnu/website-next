93d5000ae5de9ef8c9e743021461e5043623a50e
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-09-30 15:15:16 +0200

    refactor: delete unused schemas

--- src/validations/reservations/fetchManyReservationsSchema.ts
+++
@@ -1,9 +0,0 @@
-import { isBefore } from 'date-fns';
-import { z } from 'zod';
-import type { Translations } from '@/lib/locale';
-
-function fetchManyReservationsSchema(t: Translations) {
-  return z.number().min(1, t('reservations.api.invalidId')).max(50);
-}
-
-export { fetchManyReservationsSchema };
