17ecf6852839304f3bcba7942a29fc56c71de019
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-09-30 15:30:58 +0200

    refactor: update toolcard to use fetched tool values

--- src/app/[locale]/(default)/reservations/[calendarId]/page.tsx
+++ src/app/[locale]/(default)/reservations/[calendarId]/page.tsx
@@ -1,11 +1,15 @@
-import ToolCalendar from '@/components/reservations/reservations-calendar/ToolCalendar';
-
+import { notFound } from 'next/navigation';
+import { ToolCalendar } from '@/components/reservations/reservations-calendar/ToolCalendar';
+import { api } from '@/lib/api/server';
 export default async function ToolCalendarPage({
   params,
 }: {
   params: Promise<{ calendarId: string }>;
 }) {
   const { calendarId } = await params;
-  const toolId = Number(calendarId);
-  return <ToolCalendar />;
+  const { user } = await api.auth.state();
+  const tool = await api.tools.fetchTool(Number(calendarId));
+  if (!tool) return notFound();
+
+  return <ToolCalendar tool={tool} user={user} />;
 }
--- src/components/reservations/ToolCard.tsx
+++ src/components/reservations/ToolCard.tsx
@@ -1,10 +1,7 @@
-'use client';
-
 import { Maximize2Icon } from 'lucide-react';
 import { m } from 'motion/react';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
-import { useId } from 'react';
 import type { Tool } from '@/components/reservations/ToolCardGrid';
 import { Button } from '@/components/ui/Button';
 import {
@@ -14,6 +11,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/Card';
+import { toolDescriptionFields } from '@/lib/constants';
 import { Link } from '@/lib/locale/navigation';
 
 type ToolCardProps = {
@@ -24,30 +22,21 @@ type ToolCardProps = {
 function ToolCard({ tool, onClick }: ToolCardProps) {
   const t = useTranslations('reservations');
 
-  const fieldsToShow = [
-    'krever',
-    'difficulty',
-    'filamentSize',
-    'filamentType',
-    'slicer',
-  ] as const;
-
-  const id = useId();
-
   return (
     <Card className='flex h-112 w-80 flex-col gap-2 overflow-hidden rounded-xl hover:brightness-105'>
       <CardHeader className='p-0'>
         <div className='relative h-44 w-full'>
           <Image
             priority
-            src={tool.photoUrl}
-            alt={tool.title}
+            src={tool.imageUrl ?? '/unknown.png'}
+            alt={tool.name}
             fill
             className='object-cover'
           />
+
           <m.button
             className='absolute top-2 right-2 z-10 inline-flex size-11 cursor-pointer items-center justify-center rounded-full bg-stone-500 bg-opacity-50 backdrop-blur-sm ease-in-out hover:bg-primary'
-            key={`cardHeaderButton-${tool.title}-${id}`}
+            key={`cardHeaderButton-${tool.name}`}
             onClick={onClick}
             whileHover={{ scale: 1.1 }}
             title={t('tools.tooltip')}
@@ -56,32 +45,32 @@ function ToolCard({ tool, onClick }: ToolCardProps) {
           </m.button>
         </div>
         <CardTitle className='truncate text-center'>
-          <span className='clamp-[text-lg-xl-clamp]'>{tool.title}</span>
+          <span className='clamp-[text-lg-xl-clamp]'>{tool.name}</span>
           <br />
           <span className='clamp-[text-sm-base-clamp]'>{tool.nickName}</span>
         </CardTitle>
       </CardHeader>
       <CardContent className='flex flex-wrap gap-1 overflow-auto px-3'>
-        {tool.type === 'printer' &&
-          fieldsToShow.map((field) => {
-            const text = tool[field];
+        {tool.type === '3dprinter' &&
+          toolDescriptionFields.map(({ key, label }) => {
+            const text = tool[key as keyof Tool];
             return (
               <span
-                key={field}
+                key={key}
                 className='h-fit w-fit rounded-xl bg-stone-600 p-1 text-sm'
               >
-                {field.charAt(0).toUpperCase() + field.slice(1)}: {text}
+                {t(`tools.${label}`)}: {String(text)}
               </span>
             );
           })}
       </CardContent>
       <CardFooter className='mt-auto h-20 w-full p-0'>
-        {tool.type === 'printer' ? (
+        {tool.type === '3dprinter' ? (
           tool.available ? (
             <Link
               href={{
-                pathname: '/reservations/[reservationId]',
-                params: { reservationId: tool.toolId },
+                pathname: '/reservations/[calendarId]',
+                params: { calendarId: tool.toolId },
               }}
               className='mt-auto w-full'
             >
