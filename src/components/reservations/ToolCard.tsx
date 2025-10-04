16e1f913ff9aae66c3d959166f0e2272f56524ad
MoaKK <142320965+MoaKK@users.noreply.github.com>
2025-10-04 14:22:10 +0200

    refactor: fix styling and remove redundancy in toolcard

--- src/components/reservations/ToolCard.tsx
+++ src/components/reservations/ToolCard.tsx
@@ -22,6 +22,53 @@ type ToolCardProps = {
 function ToolCard({ tool, onClick }: ToolCardProps) {
   const t = useTranslations('reservations');
 
+  const button = (() => {
+    switch (tool.status) {
+      case 'available':
+        return (
+          <Link
+            href={{
+              pathname: '/reservations/[calendarId]',
+              params: { calendarId: tool.toolId },
+            }}
+            className='mt-auto w-full'
+          >
+            <Button className='clamp-[text-base-lg-clamp] h-14 w-full rounded-t-none hover:bg-primary'>
+              {t('tools.available')}
+            </Button>
+          </Link>
+        );
+      case 'requires_supervision':
+        return (
+          <Button
+            variant='secondary'
+            className='clamp-[text-base-lg-clamp] mt-auto h-14 w-full rounded-t-none'
+            disabled
+          >
+            {t('tools.supervision')}
+          </Button>
+        );
+      case 'out_of_order':
+        return (
+          <Button
+            variant='ghost'
+            className='clamp-[text-base-lg-clamp] pointer-events-none mt-auto h-14 w-full rounded-t-none rounded-b-xl'
+          >
+            {t('tools.outOfOrder')}
+          </Button>
+        );
+      default:
+        return (
+          <Button
+            variant='destructive'
+            className='clamp-[text-base-lg-clamp] pointer-events-none mt-auto h-14 w-full rounded-t-none'
+          >
+            {t('tools.unavailable')}
+          </Button>
+        );
+    }
+  })();
+
   return (
     <Card className='flex h-112 w-80 flex-col gap-2 overflow-hidden rounded-xl hover:brightness-105'>
       <CardHeader className='p-0'>
@@ -33,15 +80,14 @@ function ToolCard({ tool, onClick }: ToolCardProps) {
             fill
             className='object-cover'
           />
-
           <m.button
-            className='absolute top-2 right-2 z-10 inline-flex size-11 cursor-pointer items-center justify-center rounded-full bg-stone-500 bg-opacity-50 backdrop-blur-sm ease-in-out hover:bg-primary'
+            className='absolute top-2 right-2 z-10 inline-flex size-11 items-center justify-center rounded-full bg-stone-500/50 backdrop-blur-sm ease-in-out hover:bg-primary'
             key={`cardHeaderButton-${tool.name}`}
             onClick={onClick}
             whileHover={{ scale: 1.1 }}
             title={t('tools.tooltip')}
           >
-            <Maximize2Icon className='size-6 transform stroke-stone-300 transition delay-75 duration-300 ease-in-out hover:scale-110' />
+            <Maximize2Icon className='size-6 stroke-stone-300 transition delay-75 duration-300 ease-in-out hover:scale-110' />
           </m.button>
         </div>
         <CardTitle className='truncate text-center'>
@@ -50,51 +96,26 @@ function ToolCard({ tool, onClick }: ToolCardProps) {
           <span className='clamp-[text-sm-base-clamp]'>{tool.nickName}</span>
         </CardTitle>
       </CardHeader>
-      <CardContent className='flex flex-wrap gap-1 overflow-auto px-3'>
-        {tool.type === '3dprinter' &&
-          toolDescriptionFields.map(({ key, label }) => {
-            const text = tool[key as keyof Tool];
-            return (
+
+      <CardContent className='flex flex-1 flex-wrap gap-1 overflow-auto px-3'>
+        {toolDescriptionFields.map(({ key, label }) => {
+          const field = tool[key as keyof Tool];
+          const text = field && String(field).trim();
+
+          return (
+            text && (
               <span
                 key={key}
-                className='h-fit w-fit rounded-xl bg-stone-600 p-1 text-sm'
+                className='h-fit w-fit rounded-xl bg-secondary p-1.5 text-sm'
               >
-                {t(`tools.${label}`)}: {String(text)}
+                {t(`tools.${label}`)}: {text}
               </span>
-            );
-          })}
+            )
+          );
+        })}
       </CardContent>
-      <CardFooter className='mt-auto h-20 w-full p-0'>
-        {tool.type === '3dprinter' ? (
-          tool.available ? (
-            <Link
-              href={{
-                pathname: '/reservations/[calendarId]',
-                params: { calendarId: tool.toolId },
-              }}
-              className='mt-auto w-full'
-            >
-              <Button className='clamp-[text-base-lg-clamp] h-14 w-full rounded-t-none hover:bg-primary'>
-                {t('tools.available')}
-              </Button>
-            </Link>
-          ) : (
-            <Button
-              variant='destructive'
-              className='clamp-[text-base-lg-clamp] pointer-events-none mt-auto h-14 w-full rounded-t-none hover:bg-destructive'
-            >
-              {t('tools.unavailable')}
-            </Button>
-          )
-        ) : (
-          <Button
-            variant='secondary'
-            className='clamp-[text-base-lg-clamp] pointer-events-none mt-auto h-14 w-full rounded-t-none text-center brightness-90'
-          >
-            {t('tools.supervision')}
-          </Button>
-        )}
-      </CardFooter>
+
+      <CardFooter className='h-fit w-full p-0'>{button}</CardFooter>
     </Card>
   );
 }
