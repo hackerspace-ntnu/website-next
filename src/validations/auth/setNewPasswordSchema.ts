4d1285fcc36abb4099fe00c935ad88ac29033787
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2025-10-10 13:52:01 +0200

    fix: Regex for password containing at least one lowercase letter

--- src/validations/auth/setNewPasswordSchema.ts
+++ src/validations/auth/setNewPasswordSchema.ts
@@ -12,6 +12,7 @@ function setNewPasswordSchema(t: Translations) {
       .min(1, t('auth.form.password.required'))
       .min(8, t('auth.form.password.minLength', { count: 8 }))
       .max(50, t('auth.form.password.maxLength', { count: 50 }))
+      .regex(/[a-z]/, t('auth.form.password.lowercase'))
       .regex(/[A-Z]/, t('auth.form.password.uppercase'))
       .regex(/[^a-zA-Z0-9]/, t('auth.form.password.specialChar')),
   });
