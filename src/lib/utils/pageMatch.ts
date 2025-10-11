b52e55f2cad75c7bf9798f2ab1a85837fde314da
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2025-10-11 22:53:38 +0200

    fix: Use escape-string-regexp for now
    RegExp.escape is only supported in Node.js 24, while we are using the LTS version, which is 22 right now.
    When we upgrade to the next LTS version, we can remove the dependency.

--- src/lib/utils/pageMatch.ts
+++ src/lib/utils/pageMatch.ts
@@ -1,11 +1,18 @@
-// Utility to convert a page match string to a regex pattern
-// Page match string is of our own format where * is a wildcard and , separates options
+import escapeStringRegexp from 'escape-string-regexp';
+
+/**
+ * Utility to convert a page match string to a regex pattern
+ * Page match string is of our own format where * is a wildcard and , separates options
+ * @param pageMatch string to escape
+ * @returns escaped string
+ */
 function pageMatchToRegex(pageMatch: string) {
-  // RegExp.escape escapes all special characters, while * and , are translated to regex equivalents
+  // escapeStringRegexp escapes all special characters, while * and , are translated to regex equivalents
   // * -> \* -> .*
   // , -> \x2c -> |
   // Wrapped in ^(...)$ to match the entire path
-  return `^(${RegExp.escape(pageMatch).replaceAll('\\*', '.*').replaceAll('\\x2c', '|')})$`;
+  // TODO: Change to using RegExp.escape when it bacomes a part of the LTS version of Node.js
+  return `^(${escapeStringRegexp(pageMatch).replaceAll('\\*', '.*').replaceAll('\\x2c', '|')})$`;
 }
 
 export { pageMatchToRegex };
