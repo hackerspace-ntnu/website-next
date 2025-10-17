3fde9b9b28b91d2b34fb7fd8118d6cd8c4498521
Sander Eikemo Andreassen <180218942+seandreassen@users.noreply.github.com>
2025-10-17 17:50:17 +0200

    feat: add max length validation to item schema

--- src/validations/storage/itemSchema.ts
+++ src/validations/storage/itemSchema.ts
@@ -13,18 +13,28 @@ function itemSchema(t: Translations, categories: string[]) {
       fileNotImageError: t('storage.edit.image.fileNotImageError'),
       optional: true,
     }),
-    nameEnglish: z.string().min(1, t('storage.edit.name.required')),
-    nameNorwegian: z.string().min(1, t('storage.edit.name.required')),
-    descriptionEnglish: z.string(),
-    descriptionNorwegian: z.string(),
+    nameEnglish: z
+      .string()
+      .min(1, t('storage.edit.name.required'))
+      .max(128, t('storage.edit.name.maxLength', { count: 128 })),
+    nameNorwegian: z
+      .string()
+      .min(1, t('storage.edit.name.required'))
+      .max(128, t('storage.edit.name.maxLength', { count: 128 })),
+    descriptionEnglish: z
+      .string()
+      .max(512, t('storage.edit.description.maxLength', { count: 512 })),
+    descriptionNorwegian: z
+      .string()
+      .max(512, t('storage.edit.description.maxLength', { count: 512 })),
     locationEnglish: z
       .string()
       .min(1, t('storage.edit.location.required'))
-      .max(50, t('storage.edit.location.invalid')),
+      .max(256, t('storage.edit.location.maxLength', { count: 256 })),
     locationNorwegian: z
       .string()
       .min(1, t('storage.edit.location.required'))
-      .max(50, t('storage.edit.location.invalid')),
+      .max(256, t('storage.edit.location.maxLength', { count: 256 })),
     categoryName:
       categories.length > 1
         ? z.enum(categories as [string, ...string[]])
