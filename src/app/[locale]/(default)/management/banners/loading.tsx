1e091e0f2bff4a16fffe475226302868b48f011e
Sander Eikemo Andreassen <180218942+seandreassen@users.noreply.github.com>
2025-10-19 01:25:18 +0200

    feat: add back button to banners loading

--- src/app/[locale]/(default)/management/banners/loading.tsx
+++ src/app/[locale]/(default)/management/banners/loading.tsx
@@ -1,4 +1,4 @@
-import { EditIcon, PlusIcon } from 'lucide-react';
+import { ArrowLeftIcon, EditIcon, PlusIcon } from 'lucide-react';
 import { getTranslations } from 'next-intl/server';
 import { Button } from '@/components/ui/Button';
 import { Checkbox } from '@/components/ui/Checkbox';
@@ -15,81 +15,94 @@ import {
 
 export default async function BannersLoading() {
   const t = await getTranslations('management.banners');
+  const tManagement = await getTranslations('management');
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className='w-full'>{t('content')}</TableHead>
-          <TableHead className='w-full'>{t('pages')}</TableHead>
-          <TableHead className='w-full'>{t('expiresAt')}</TableHead>
-          <TableHead className='w-full'>{t('active')}</TableHead>
-          <TableHead className='min-w-20'>
-            <Link
-              href='/management/banners/new'
-              variant='default'
-              size='icon'
-              className='my-2'
-              aria-label={t('create')}
-            >
-              <PlusIcon />
-            </Link>
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell>
-            <Skeleton className='h-[18px] w-7/12' />
-          </TableCell>
-          <TableCell>
-            <Skeleton className='h-[18px] w-1/4' />
-          </TableCell>
-          <TableCell>-</TableCell>
-          <TableCell>
-            <Checkbox />
-          </TableCell>
-          <TableCell>
-            <Button size='icon'>
-              <EditIcon />
-            </Button>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <Skeleton className='h-[18px] w-2/3' />
-          </TableCell>
-          <TableCell>
-            <Skeleton className='h-[18px] w-1/4' />
-          </TableCell>
-          <TableCell>-</TableCell>
-          <TableCell>
-            <Checkbox />
-          </TableCell>
-          <TableCell>
-            <Button size='icon'>
-              <EditIcon />
-            </Button>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <Skeleton className='h-[18px] w-1/2' />
-          </TableCell>
-          <TableCell>
-            <Skeleton className='h-[18px] w-1/4' />
-          </TableCell>
-          <TableCell>-</TableCell>
-          <TableCell>
-            <Checkbox />
-          </TableCell>
-          <TableCell>
-            <Button size='icon'>
-              <EditIcon />
-            </Button>
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <>
+      <Link
+        href='/management'
+        className='flex w-fit gap-2'
+        variant='ghost'
+        size='default'
+        aria-label={tManagement('backToManagement')}
+      >
+        <ArrowLeftIcon aria-hidden='true' />
+        {tManagement('backToManagement')}
+      </Link>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className='w-full'>{t('content')}</TableHead>
+            <TableHead className='w-full'>{t('pages')}</TableHead>
+            <TableHead className='w-full'>{t('expiresAt')}</TableHead>
+            <TableHead className='w-full'>{t('active')}</TableHead>
+            <TableHead className='min-w-20'>
+              <Link
+                href='/management/banners/new'
+                variant='default'
+                size='icon'
+                className='my-2'
+                aria-label={t('create')}
+              >
+                <PlusIcon />
+              </Link>
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <Skeleton className='h-[18px] w-7/12' />
+            </TableCell>
+            <TableCell>
+              <Skeleton className='h-[18px] w-1/4' />
+            </TableCell>
+            <TableCell>-</TableCell>
+            <TableCell>
+              <Checkbox />
+            </TableCell>
+            <TableCell>
+              <Button size='icon'>
+                <EditIcon />
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <Skeleton className='h-[18px] w-2/3' />
+            </TableCell>
+            <TableCell>
+              <Skeleton className='h-[18px] w-1/4' />
+            </TableCell>
+            <TableCell>-</TableCell>
+            <TableCell>
+              <Checkbox />
+            </TableCell>
+            <TableCell>
+              <Button size='icon'>
+                <EditIcon />
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <Skeleton className='h-[18px] w-1/2' />
+            </TableCell>
+            <TableCell>
+              <Skeleton className='h-[18px] w-1/4' />
+            </TableCell>
+            <TableCell>-</TableCell>
+            <TableCell>
+              <Checkbox />
+            </TableCell>
+            <TableCell>
+              <Button size='icon'>
+                <EditIcon />
+              </Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </>
   );
 }
