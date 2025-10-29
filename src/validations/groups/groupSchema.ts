33c645dfbb800439cd086a8a0b1f91b5f1b01766
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2025-10-29 23:36:36 +0100

    fix: Make sure input for leader and deputy is a number

--- src/validations/groups/groupSchema.ts
+++ src/validations/groups/groupSchema.ts
@@ -32,8 +32,8 @@ function groupSchema(t: Translations) {
       .max(255, t('groups.form.summary.maxLength', { count: 255 })),
     descriptionNorwegian: plateValueSchema,
     descriptionEnglish: plateValueSchema,
-    leaderId: z.string().nullable(),
-    deputyLeaderId: z.string().nullable(),
+    leaderId: z.coerce.number().nullable(),
+    deputyLeaderId: z.coerce.number().nullable(),
     identifier: z
       .string()
       .min(1, t('groups.form.identifier.required'))
