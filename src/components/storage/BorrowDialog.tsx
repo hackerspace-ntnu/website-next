a66370183abb4eeab836e83e320f2fa7a5b4b1f0
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2025-11-15 15:49:07 +0100

    fix: Always calculate disabled days for LoanForm

--- src/components/storage/BorrowDialog.tsx
+++ src/components/storage/BorrowDialog.tsx
@@ -47,7 +47,7 @@ function BorrowDialog({ t, className, isLoggedIn }: BorrowDialogProps) {
   );
 
   const disabledDays = api.storage.calculateDisabledDays.useQuery(cart ?? [], {
-    enabled: isLoggedIn && itemsInCart.data && loanInAdvance && !!cart,
+    enabled: isLoggedIn && itemsInCart.data && !!cart,
   });
 
   return (
