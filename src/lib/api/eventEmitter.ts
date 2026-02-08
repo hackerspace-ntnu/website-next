f9098d9acdd5f1fa03d23babf1dfd4c458c41674
CraZyB1336 <chrisbraat5@gmail.com>
2026-02-08 13:43:42 +0100

    fix: hot reload for eventemitter

--- src/lib/api/eventEmitter.ts
+++ src/lib/api/eventEmitter.ts
@@ -1,4 +1,14 @@
 import 'server-only';
 import { EventEmitter } from 'node:events';
 
-export const eventEmitter = new EventEmitter();
+// This fixes hot reload issue in dev mode.
+const globalEventEmitter = globalThis as unknown as {
+  __eventEmitter?: EventEmitter;
+};
+
+// Akin to a singleton.
+if (!globalEventEmitter.__eventEmitter) {
+  globalEventEmitter.__eventEmitter = new EventEmitter();
+}
+
+export const eventEmitter = globalEventEmitter.__eventEmitter;
