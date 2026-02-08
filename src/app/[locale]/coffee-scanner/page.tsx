dfdb29971ed4ea8c84c6e35d0573339b7da54435
CraZyB1336 <chrisbraat5@gmail.com>
2026-02-08 22:10:25 +0100

    feat: design inactive state

--- src/app/[locale]/coffee-scanner/page.tsx
+++ src/app/[locale]/coffee-scanner/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { HackerspaceLogo } from '@/components/assets/logos';
 import { api } from '@/lib/api/client';
 
 export default function CoffeePage() {
@@ -19,6 +20,22 @@ export default function CoffeePage() {
     },
   });
 
+  if (currentCardId === '') {
+    return (
+      <main className='absolute top-0 left-0 h-full w-full'>
+        <h1 className='-translate-x-1/2 -translate-y-1/2 absolute top-5/12 left-1/2 w-max max-w-9/10 text-center text-7xl'>
+          Scan your student card before receiving coffee
+        </h1>
+        <div className='-translate-x-1/2 absolute bottom-0 left-1/2 mb-30 flex flex-row gap-2 text-4xl'>
+          Provided by
+          <div className='flex flex-row'>
+            <HackerspaceLogo className='h-12 w-12' />
+            Hackerspace
+          </div>
+        </div>
+      </main>
+    );
+  }
   return (
     <div>
       <h1>Hi there, this is the coffee page.</h1>
