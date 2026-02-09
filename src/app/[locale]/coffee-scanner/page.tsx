229670c149dc40af6f5b5784e7a82c8c0177bae8
CraZyB1336 <chrisbraat5@gmail.com>
2026-02-09 21:49:28 +0100

    feat: add shimmer effect

--- src/app/[locale]/coffee-scanner/page.tsx
+++ src/app/[locale]/coffee-scanner/page.tsx
@@ -20,17 +20,32 @@ export default function CoffeePage() {
     },
   });
 
+  // [#EBBE9B]
+
   if (currentCardId === '') {
     return (
       <main className='absolute top-0 left-0 h-full w-full'>
-        <h1 className='-translate-x-1/2 -translate-y-1/2 absolute top-5/12 left-1/2 w-max max-w-9/10 text-center text-7xl'>
-          Scan your student card before receiving coffee
+        <h1 className='-translate-x-1/2 -translate-y-1/2 absolute top-2/12 left-1/2 w-max max-w-9/10 text-center font-extrabold text-7xl'>
+          Want some{' '}
+          <span className='relative inline-block text-[#5E3023]'>
+            coffee?
+            <span
+              aria-hidden='true'
+              className='pointer-events-none absolute inset-0 animate-shimmer bg-[length:200%_100%] bg-[linear-gradient(110deg,transparent_42%,#EBBE9Bcc_45%,#F5F9E9cc_50%,#EBBE9Bcc_55%,transparent_58%)] bg-clip-text text-transparent'
+            >
+              coffee?
+            </span>
+          </span>
         </h1>
-        <div className='-translate-x-1/2 absolute bottom-0 left-1/2 mb-30 flex flex-row gap-2 text-4xl'>
-          Provided by
-          <div className='flex flex-row'>
+        <h1 className='-translate-x-1/2 -translate-y-1/2 absolute top-5/12 left-1/2 w-max max-w-9/10 text-center font-normal text-6xl'>
+          Scan your <span className='font-extrabold'>student card</span> before
+          grabbing a drink
+        </h1>
+        <div className='-translate-x-1/2 absolute bottom-0 left-1/2 mb-10 flex flex-row items-center gap-2 text-2xl'>
+          <span className='font-montserrat'>PROVIDED BY</span>
+          <div className='flex flex-row items-center'>
             <HackerspaceLogo className='h-12 w-12' />
-            Hackerspace
+            <span className='font-bold font-montserrat'> HACKERSPACE</span>
           </div>
         </div>
       </main>
