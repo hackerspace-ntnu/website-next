6d1fc1cd8cda8c35380b54e4976e830777748ee5
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2026-05-21 14:01:12 +0200

    fix: Update Plate value schema

--- src/validations/plate.ts
+++ src/validations/plate.ts
@@ -1,25 +1,14 @@
 import { z } from 'zod';
 
-const baseElementSchema = z
-  .object({
-    type: z.string(),
-  })
-  .passthrough();
+const textSchema = z.looseObject({
+  text: z.string(),
+});
 
-const textSchema = z
-  .object({
-    text: z.string(),
-  })
-  .passthrough();
-
-type Text = z.infer<typeof textSchema>;
-
-type Element = z.infer<typeof baseElementSchema> & {
-  children: (Element | Text)[];
-};
-
-const elementSchema: z.ZodType<Element> = baseElementSchema.extend({
-  children: z.lazy(() => z.union([elementSchema, textSchema]).array()),
+const elementSchema = z.looseObject({
+  type: z.string(),
+  get children() {
+    return z.array(z.union([elementSchema, textSchema]));
+  },
 });
 
 const plateValueSchema = z.array(elementSchema);
